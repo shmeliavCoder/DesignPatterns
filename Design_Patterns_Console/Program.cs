96b21b4c34bd8a0ae330c7c23be1a11f91d12e01
ripper963 <ripper963@gmail.com>
2019-07-14 10:22:19 +0300

    Initial check-in of module Design_Patterns_Console

---
+++ Design_Patterns_Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Design_Patterns_Console
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
