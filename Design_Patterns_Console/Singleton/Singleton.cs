56372d5cbb261f6662136d1ef6555cf189e88b22
ripper963 <ripper963@gmail.com>
2019-07-14 12:45:50 +0300

    renamed singleton folder

---
+++ Design_Patterns_Console/Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Design_Patterns_Console.Class1
+{
+    public class Singleton
+    {
+        private static Singleton INSTANCE;
+        private static object key = new Object();
+
+        private Singleton()
+        {
+
+        }
+
+        public static Singleton GetInstance()
+        {
+            if(INSTANCE != null)
+            {
+                return INSTANCE;
+            }
+
+            lock (key)
+            {
+                if (INSTANCE == null)
+                {
+                    INSTANCE = new Singleton();
+                }
+            }
+
+            return INSTANCE;
+        }
+    }
+}
